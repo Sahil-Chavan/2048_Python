a9da915cd84862d227d7b6af1bfd874068d7c002
Sahil-Chavan <sahiledupor@gmail.com>
2022-02-07 12:39:21 +0530

    feature: Display Board
    Here I have added an feature to display the game board or in technical terms display the matrix.

--- 2048.py
+++ 2048.py
@@ -1,4 +1,16 @@
-GRID = [[0]*3 for _ in range(3)]
 
-print('')
-print('\n\n'.join([' '.join(['{:5}'.format(item) for item in row]) for row in GRID]))
+
+class Python_2048():
+    def __init__(self,board_size,game_type):
+        n = int(board_size)*4
+        self.GRID = [[0]*n for _ in range(n)]
+
+    def Display_board(self):
+        print('')
+        print('\n\n'.join([' '.join(['{:5}'.format(item) for item in row]) for row in self.GRID]))
+        
+    # def Merge_right_row():
+        
+if __name__ == '__main__':
+    game = Python_2048(2,'NA')
+    game.Display_board()

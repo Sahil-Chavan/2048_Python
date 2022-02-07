762674f88fced9de5f485e3afd0e8f0f861a01c4
Sahil-Chavan <sahiledupor@gmail.com>
2022-02-07 13:06:46 +0530

    feature: Options and Move's structure.
    Here I have added the structure for providing the user with options to play and Make_move function.

--- 2048.py
+++ 2048.py
@@ -1,16 +1,36 @@
 
 
-class Python_2048():
+class Game():
     def __init__(self,board_size,game_type):
         n = int(board_size)*4
-        self.GRID = [[0]*n for _ in range(n)]
+        self.Grid = [[0]*n for _ in range(n)]
 
-    def Display_board(self):
+    @staticmethod
+    def Display_board(grid):
         print('')
-        print('\n\n'.join([' '.join(['{:5}'.format(item) for item in row]) for row in self.GRID]))
+        print('\n\n'.join([' '.join(['{:5}'.format(item) for item in row]) for row in grid]))
+   
+    def Provide_options(self):
+        print('options:')
+        option = int(input)
+        self.Make_move(option)
+    
+    def Make_move(self,option):
+        self.New_Grid = self.Grid.copy()
+        ## changes in grid
+        if self.New_Grid==self.Grid:
+            print('\t\t INVALID MOVE ! - Since it causes no changes in the grid')
+            self.Move()
+        else:
+            self.Grid = self.New_Grid
+            Game.Display_board(self.Grid)
+            
+    def Merge_left_matrix(grid):
+       for r in grid:
+           temp_row = r.copy()
+           
         
-    # def Merge_right_row():
         
 if __name__ == '__main__':
-    game = Python_2048(2,'NA')
+    game = Game(2,'NA')
     game.Display_board()

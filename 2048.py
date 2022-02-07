f167fbfecd67a33fd45cc4013a139a3c77dffde0
Sahil-Chavan <sahiledupor@gmail.com>
2022-02-07 18:59:33 +0530

    feature: Adding Comments
    Added appropriate comments in the code, to make it more readable and interpretable.

--- 2048.py
+++ 2048.py
@@ -2,15 +2,16 @@ import copy
 from random import sample
 
 class Game():
+    # Initializing the constructor
     def __init__(self):
-        
         self.Board_size,self.Game_type = Game.Collect_size()
         self.Grid = [[0]*self.Board_size for _ in range(self.Board_size)]
         self.Random_add()
-        # self.Grid = [[2,7,1,9],[4,0,5,0],[1,4,3,2],[2,1,7,8]]
+        # self.Grid = [[2,7,1,9],[4,0,5,0],[1,4,3,2],[1024,1024,1024,1024]]
         self.Function_map = {1:Game.Merge_left_matrix,2:Game.Merge_right_matrix,3:Game.Merge_up_matrix,4:Game.Merge_down_matrix }
         self.Provide_options()
     
+    # An static method for taking board size and win condition from user.
     @staticmethod
     def Collect_size():
         got_option1 = False
@@ -35,6 +36,7 @@ class Game():
             
         return board_size*4,game_type*2048
         
+    # An static method for considering the next move from the user.
     @staticmethod
     def Collect_move():
         got_option = False
@@ -52,11 +54,13 @@ class Game():
             exit()
         return option
     
+    # An static method for displaying the game board.
     @staticmethod
     def Display_board(grid):
         print('')
         print('\n\n'.join([' '.join(['{:5}'.format(item) for item in row]) for row in grid]))
    
+    # An method for displaying the the options of possible moves.
     def Provide_options(self,with_random_add =True):
         if with_random_add:self.Random_add()
         self.Verdict()
@@ -64,6 +68,7 @@ class Game():
         option = Game.Collect_move()
         self.Make_move(option)
     
+    #Metho for calling the specific method for specific move and validating the move.
     def Make_move(self,option):
         
         self.New_Grid = copy.deepcopy(self.Grid)
@@ -71,29 +76,24 @@ class Game():
         
         if self.New_Grid==self.Grid:
             print('\tINVALID MOVE ! - Since it causes no changes in the grid \n \tProvide with new option')
-            # Game.Display_board(self.Grid)
             self.Provide_options(False)
         else:
             self.Grid = copy.deepcopy(self.New_Grid)
             
             self.Provide_options()
-            # Game.Display_board(self.Grid)
     
+    # Method for checking the Win or Lose conditions.
     def Verdict(self):
-        # print('checking for verdict1')
         self.Has_won()
-        # print('checking for verdict2')
         is_empty = False
         for r in range(self.Board_size):
             for c in range(self.Board_size):
                 if self.Grid[r][c] == 0:
                     is_empty = True
-        # print('checking for verdict3',is_empty) 
         if not is_empty:
-            print('checking for lost')
             self.Has_lost()
 
-        
+    # Method for Losing conditions
     def Has_lost(self):
         test_grid = copy.deepcopy(self.Grid)
         if self.Function_map[1](test_grid)==test_grid:
@@ -104,6 +104,7 @@ class Game():
                         print('\t !!! You Lost !!! \n\t !!! Thank you for playing !!!')
                         exit()
     
+    # Method for Wining conditions
     def Has_won(self):
         for r in range(self.Board_size):
             for c in range(self.Board_size):
@@ -112,6 +113,7 @@ class Game():
                     print('\t !!! Hurray You Won !!! \n\t !!! Thank you for playing !!!')
                     exit()
         
+    # Static Method for computing the matrix after sliding in left direction.
     @staticmethod
     def Merge_left_matrix(grid):
         for row in grid:
@@ -130,12 +132,14 @@ class Game():
             row[:] = new_row[:]
         return grid
     
+    # Static Method for computing the matrix after sliding in right direction.
     @staticmethod
     def Merge_right_matrix(grid):
         grid_r =  [list(reversed(l)) for l in grid]
         grid_l = Game.Merge_left_matrix(grid_r)
         return [list(reversed(l)) for l in grid_l]
     
+    # Static Method for computing the matrix after sliding in upward direction.
     @staticmethod
     def Merge_up_matrix(grid):
         grid_u = list()
@@ -149,6 +153,7 @@ class Game():
             grid_u.append(list(row)) 
         return grid_u
     
+    # Static Method for computing the matrix after sliding in downward direction.
     @staticmethod
     def Merge_down_matrix(grid):
         grid_d = list()
@@ -162,6 +167,7 @@ class Game():
             grid_d.append(list(row)) 
         return grid_d
     
+    # Method for randomly adding 2 or 4 at an random position in the grid.
     def Random_add(self):
         flat_grid = [item for row in self.Grid for item in row]
         zero_indexes = [i for i,v in enumerate(flat_grid) if v==0]
@@ -171,6 +177,7 @@ class Game():
         rand_val = sample([2,4],1)[0]
         self.Grid[row_index][col_index] = rand_val
         
+    # An static method for checking is user has enterd an correct option.
     @staticmethod    
     def Check_options(extent,current_option):
         return current_option in list(range(1,extent+1))
@@ -180,7 +187,7 @@ class Game():
         
         
 if __name__ == '__main__':
-    game = Game()
+    game = Game() #Initializing the game.
     
 
     

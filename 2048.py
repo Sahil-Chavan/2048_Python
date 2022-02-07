a93dd9b8dd70977ef6304683922d1e6f4131ed26
Sahil-Chavan <sahiledupor@gmail.com>
2022-02-07 15:31:41 +0530

    feature: Upward and Downward Sliding Added
    The ability to slide the matrix Upward and Downward has been added.

--- 2048.py
+++ 2048.py
@@ -21,7 +21,7 @@ class Game():
         
         self.New_Grid = copy.deepcopy(self.Grid)
         ## changes in grid
-        self.New_Grid = Game.Merge_right_matrix(self.New_Grid)
+        self.New_Grid = Game.Merge_down_matrix(self.New_Grid)
         
         Game.Display_board(self.Grid)
         print('-'*20)
@@ -39,8 +39,8 @@ class Game():
             
     @staticmethod
     def Merge_left_matrix(grid):
-        for r in grid:
-            temp_row = list(filter(lambda x: x!=0, r))[:]
+        for row in grid:
+            temp_row = list(filter(lambda x: x!=0, row))[:]
             new_row = list()
             while temp_row:
                 curr_val = temp_row.pop(0)
@@ -51,31 +51,56 @@ class Game():
                     new_row.append(curr_val*2)
                 else:
                     new_row.append(curr_val)
-            new_row.extend([0 for _ in range(len(r)-len(new_row))])
-            r[:] = new_row[:]
+            new_row.extend([0 for _ in range(len(row)-len(new_row))])
+            row[:] = new_row[:]
         return grid
     
+    @staticmethod
     def Merge_right_matrix(grid):
         grid_r =  [list(reversed(l)) for l in grid]
-        # print(grid_r)
         grid_l = Game.Merge_left_matrix(grid_r)
-        print(grid_l)
         return [list(reversed(l)) for l in grid_l]
     
-                
+    @staticmethod
+    def Merge_up_matrix(grid):
+        grid_u = list()
+        for row in zip(*grid):
+            grid_u.append(list(row)) 
+        grid_u.reverse()
+        grid_l = Game.Merge_left_matrix(grid_u)
+        grid_l.reverse()
+        grid_u = list()
+        for row in zip(*grid_l):
+            grid_u.append(list(row)) 
+        return grid_u
+    
+    @staticmethod
+    def Merge_down_matrix(grid):
+        grid_d = list()
+        for row in zip(*grid):
+            grid_d.append(list(row)) 
+        grid_d = [list(reversed(l)) for l in grid_d]
+        grid_l = Game.Merge_left_matrix(grid_d)
+        grid_l = [list(reversed(l)) for l in grid_l]
+        grid_d = list()
+        for row in zip(*grid_l):
+            grid_d.append(list(row)) 
+        return grid_d
+        
                    
            
         
         
 if __name__ == '__main__':
-    # game = Game(1,'NA')
+    board_size = input('\tPlease select Grid size : \n\t 1) 4 X 4 Grid \n\t 2) 8 X 8 Grid')
+    game = Game(board_size,0)
     # game.Display_board()
     
     # print([[1,2,3],[1,4,5]]==[[1,2,3],[1,4,5]])
-    Grid = [[0,7,0,0],[4,0,4,2],[4,4,0,2],[8,8,4,4]]
-    b = list()
-    for a in zip(*Grid):
-        b.append(list(a))
-    b.reverse() 
-    for c in b:
-        print(c)
+    # Grid = [[0,7,0,0],[4,0,4,2],[4,4,0,2],[8,8,4,4]]
+    # b = list()
+    # for a in zip(*Grid):
+    #     b.append(list(a))
+    # b = [list(reversed(l)) for l in b]
+    # for c in b:
+    #     print(c)
